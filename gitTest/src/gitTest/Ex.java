4526b8766f740d71a4c5ff5145f6848202cd1370
EZEN <EZEN@DESKTOP-Q4JTA01>
2022-11-01 10:35:03 +0900

    update

--- gitTest/src/gitTest/Ex.java
+++ gitTest/src/gitTest/Ex.java
@@ -4,5 +4,8 @@ public class Ex {
 	public static void main(String[] args) {
 		// main branch
 		// #1
+		String url = "jdbc:mysql://database-1.cioj5rpfwjqk.ap-northeast-2.rds.amazonaws.com:3306/myProject";
+		String user = "admin";
+		String password = "";
 	}
 }
